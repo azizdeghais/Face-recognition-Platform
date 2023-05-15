c7e07fdaff13d311128eda68820d72a655e7c60b
Aziz Deghais <deghaisaziz@gmail.com>
2023-05-15 23:27:48 +0200

    push

--- back-end/.history/index_20230515201438.js
+++
@@ -1,7 +0,0 @@
-onst express = require('express');
-const app=express()
-const mongoose = require('mongoose');
-const dotenv=require('dotenv');
-const multer=require('multer')
-const ImageModel=require('./models/ImageModel')
-const cors = require('cors')
---
+++ back-end/.history/index_20230515225038.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://miker:miker@imagegeneratordall-e.arsxo0m.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
+
+// Define a schema and model for storing image URLs
+const imageSchema = new mongoose.Schema({
+  url:String,
+  nom:String,
+  prenom:String,
+  numero:String,
+  naissance:String,
+  debut:String,
+  fin:String,
+  email:String
+
+
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+// Define an endpoint for uploading images
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    // Upload image to Cloudinary
+    const cloudinary = require('cloudinary').v2;
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    // Save image URL to MongoDB
+    const image = new Image({ url: result.secure_url });
+    await image.save();
+
+    // Return the image URL in the response
+    res.json({ url: result.secure_url });
+  } catch (error) {
+    console.error('Error uploading image', error);
+    res.status(500).json({ error: 'Failed to upload image' });
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
