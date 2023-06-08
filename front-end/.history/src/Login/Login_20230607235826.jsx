a7cce3ee30be4ad69e5b2e8839b32678ac4dac82
Aziz Deghais <deghaisaziz@gmail.com>
2023-06-08 02:14:43 +0200

    last push

---
+++ front-end/.history/src/Login/Login_20230607235826.jsx
@@ -0,0 +1,51 @@
+import {useState,useEffect} from 'react'
+import { Grid,TextField,Button } from '@mui/material'
+import { useAuth } from '../Auth/Auth';
+import { useNavigate,useLocation} from 'react-router-dom';
+function Login() {
+    
+    const auth=useAuth()
+    const navigateTo=useNavigate();
+    const location = useLocation();
+
+
+    const [username,setUsername]=useState('');
+    const [password,setPassword]=useState('');
+
+    useEffect(() => {
+        const storedUsername = localStorage.getItem('username');
+        if (storedUsername) {
+          setUsername(storedUsername);
+          auth.login(storedUsername);
+
+        } else if (location.pathname !== '/login') {
+            navigateTo('/login');
+          }
+      }, [auth,location,navigateTo]);
+
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      if(username==='admin' && password==='adminadmin') {
+        localStorage.setItem('username', username);
+        auth.login(username);
+        navigateTo('/liste');
+      }
+
+    }
+  return (
+    <>
+    <Grid container>
+    <Grid container></Grid>
+    <Grid mb={2}>
+    <TextField onChange={(e)=>setUsername(e.target.value)} value={username} type='text' name='username'  variant="outlined" />
+    </Grid>
+    <Grid mb={2}>
+    <TextField onChange={(e)=>setPassword(e.target.value)}  type='password' name='password' label="Mot de passe" variant="outlined" />
+    </Grid>
+    <Button onClick={handleSubmit} variant='outlined'>Submit</Button>
+    </Grid>
+  </>
+  )
+}
+
+export default Login
